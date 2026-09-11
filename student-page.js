5f7d98b3fa837f4b93a204bb3b1dd45b74f6cb83
Bradfordsdcc <bradford.sdcc@gmail.com>
2026-09-10 22:14:12 -0700

    Update student-page.js

--- student-page.js
+++ student-page.js
@@ -26,7 +26,7 @@
 
     // Per-reaction overrides. Keys match data-react values.
     cooldowns: {
-      holymoly: 30 * 60 * 1000,   // 30 minutes
+      holymoly: 1 * 60 * 1000,   // 30 minutes
     },
   };
 
